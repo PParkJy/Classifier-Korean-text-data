8ae3628a5b20b05f062b3991bae9165123ed76b1
PParkJy <wldus8677@gmail.com>
2020-05-13 19:16:04 +0900

    docs: temp save2

--- twitch_chat/laugh_trans.py
+++ twitch_chat/laugh_trans.py
@@ -5,65 +5,30 @@
 길이 3 이상일 경우 ㅋ 시퀀스의 길이 3으로 통일
 '''
 
-import math
+import math, glob
 import csv
 from soynlp.word import WordExtractor
 
-file_name = './data/non_highlight/79match1/399807785_'
-
-def read_data(filename):
+def read_data():
     raw_time = []
     raw_chat = []
-    for i in range (1,28):
-        fname = filename + str(i) + ".csv"
-        f = open(fname, 'r', encoding='euc-kr')
+    for i in glob.glob("preprocessed/*.csv"):
+        f = open(i,"r",encoding='utf-8')
         raw = csv.reader(f)
         for line in raw:
-            raw_time.append(line[1])
-            raw_chat.append(line[2])
+            raw_time.append(line[0])
+            raw_chat.append(line[1])
         f.close()
     return raw_time, raw_chat
 
-
-def laugh_trans(raw_chat):
-    trans_raw = []
-    for chat in raw_chat:
-        laugh_len = chat.count("ㅋ")
-        if laugh_len: #ㅋ이 있다면
-            #ㅋ이 3개 미만이면 소거, 3개 이상이면 3개로 통일
-            idx = 0
-            laugh_cnt = 0
-            chat2 = list(chat)
-            chat = chat + "_"
-            for i in chat:
-                if i == "ㅋ":
-                    laugh_cnt += 1
-                else:
-                    if laugh_cnt > 0 and laugh_cnt < 3:
-                        for j in range(idx - laugh_cnt, idx):
-                            chat2[j] = "*"
-                    else:
-                        for j in range(idx - laugh_cnt, idx-3):
-                            chat2[j] = "*"
-                    laugh_cnt = 0
-                idx += 1
-            chat2 = ''.join(list(filter(("*").__ne__, chat2)))
-            laugh_len = chat2.count("ㅋ")
-            chat2 = chat2.replace("ㅋ", "", laugh_len - 3)
-            trans_raw.append(chat2)
-    return trans_raw
-
-raw_time, raw_chat = read_data(file_name)
-trans_chat = laugh_trans(raw_chat)
-print(trans_chat)
-
 word_extractor = WordExtractor(
-    min_frequency=20,
+    min_frequency=200,
     min_cohesion_forward=0.05,
     min_right_branching_entropy=0.0
 ) #여기서는 Cohesion Score 사용
 
-word_extractor.train(trans_chat)
+raw_time, raw_chat = read_data()
+word_extractor.train(raw_chat)
 words = word_extractor.extract()
 
 def word_score(score):
